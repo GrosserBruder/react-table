dbd394cdc13c2d8913a8201682f7104d4bfc3573
Иван Лещинский <falenangellll@yandex.ru>
2022-06-23 14:23:39 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ export { default as HeadCell } from "./HeadCell";
 export { default as Head } from "./Head";
 export { default as Row } from "./Row";
 export { default as EditableCell } from "./EditableCell";
+
+export type { TableProps } from "./Table"
+export type { BodyProps } from "./Body";
+export type { CellProps } from "./Cell";
+export type { HeadCellProps } from "./HeadCell";
+export type { HeadProps } from "./Head";
+export type { RowProps } from "./Row";
+export type { EditableCellProps } from "./EditableCell";
